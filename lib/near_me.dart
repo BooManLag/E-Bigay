d208204417a5abe871357b519f2abf59f3ae4fd8
BooManLag <bumanlag315@gmail.com>
2022-03-07 01:41:00 +0800

    with near me na tapos medyo may android thingy

--- lib/near_me.dart
+++ lib/near_me.dart
@@ -1,15 +1,47 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NearMe extends StatefulWidget {
-  const NearMe({Key? key}) : super(key: key);
-
   @override
-  _NearMeState createState() => _NearMeState();
+  State<NearMe> createState() => NearMeState();
 }
 
-class _NearMeState extends State<NearMe> {
+class NearMeState extends State<NearMe> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
